8ea64ac7588db54e9d5b77470cb3bdb07cc35cc4
horndjellyfish <league@WTS-MM-7.local>
2017-12-21 18:31:08 -0800

    christmas tree ip
    12/21/17

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -10,7 +10,7 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class ChristmasTree {
 
-	Robot rob  = new Robot();
+	Robot max  = new Robot();
 	
     public static void main(String[] args) {
         ChristmasTree ohChristmasTree = new ChristmasTree();
@@ -27,54 +27,66 @@ public class ChristmasTree {
     void drawTreeBody() {
         // 8. Change the color of the line the robot draws to forest green    
 
-
+max.setPenColor( 000, 136, 136);
         // 1. Make a variable for turnAmount and set it to 175
-
+int turnAmount = 175;
 
         // 2. Start the Robot facing to the right
 
-
+max.turn(90);
         // 5. Repeat steps 3 through 11, 11 times
+for (int i = 0; i < 11; i++) {
+	
 
 
             // 3. Move the robot the width of the tree
-    
+    max.move((int) treeWidth);
             // 4. Turn the robot the current turnAmount to the right
-    
+    max.turn(turnAmount);
             // 6. Set the treeWidth to the current treeWidth times the scale
-    
+    treeWidth = treeWidth * scale ;
             // 7. Move the robot the width of a tree again
-    
+    max.move((int) treeWidth);
             // 9. Turn the robot the current turn amount to the LEFT
-    
+    max.turn(-turnAmount);
             // 10. Set the treeWidth to the current treeWidth times the scale again
-    
+    treeWidth = ((int) treeWidth * scale);
             // 11. Decrease turnAmount by 1
-
-
+turnAmount--;
+    }
 }
     
     void drawTreeTrunk() {
         // 1. Move the robot half the width of the tree
-
+max.move((int)treeWidth/2);
 
         // 2. Change the robot so that it is pointing straight down
-
+max.setAngle(180);
 
         // 4. Set the pen width to the tree width divided by 10
-
+ = treeWidth/10; 
 
         // 5. Change the color of the line the robot draws to brown
-
+max.setPenColor(  187,102,68);
 
         // 3. Move the robot a quarter the tree width
+max.move((int)treeWidth/4);
 
 
     }
     
     void drawStar() {
         // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+max.miniaturize();    	
+max.setSpeed(100);
+   max.penDown();
+   for (int i = 0; i <5; i++) {
+	max.move(100);
+    	max.turn(144);
+}
+   
+    	
+   
     }
 
 
